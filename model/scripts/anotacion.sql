aa940feaaeacaf17c173bf65c4ef20c63d1e2ad4
usuario <usuario@pc211-3>
2016-04-04 14:56:04 +0200

    Añadimos fichero .gitignore e implementamos la tarea #3

--- model/scripts/anotacion.sql
+++ model/scripts/anotacion.sql
@@ -1,6 +1,9 @@
+
+
 -- Describe ANOTACION
-CREATE TABLE "anotacion" (
-    "ID_ALUMNO" INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE anotacion (
+"ID" INTEGER PRIMARY KEY ,
+    "ID_ALUMNO" INTEGER,
     "Fecha" TEXT,
     "Hora" TEXT,
     "Descripcion" TEXT
