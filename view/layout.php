87bc5bda6f79b0d325fdfd784d95a42c34e5601f
usuario <usuario@pc79-1.(none)>
2016-04-07 13:38:44 +0200

    cambiado ubicacion calendario

--- view/layout.php
+++ view/layout.php
@@ -10,14 +10,14 @@
 			<link href="/css/bootstrap-combined.min.css" rel="stylesheet">
 			<link href="/css/bootstrap-theme.min.css" rel="stylesheet">
 			<link href="/css/jumbotron-narrow.css" rel="stylesheet">
-			<link href="calendario_dw/calendario_dw-estilos.css" type="text/css" rel="STYLESHEET">
+			<link href="css/calendario_dw/calendario_dw-estilos.css" type="text/css" rel="STYLESHEET">
    <style type="text/css">
    body{
       font-family: tahoma, verdana, sans-serif;
    }
    </style>
-   <script type="text/javascript" src="calendario_dw/jquery-1.4.4.min.js"></script>
-   <script type="text/javascript" src="calendario_dw/calendario_dw.js"></script>
+   <script type="text/javascript" src="js/jquery-1.4.4.min.js"></script>
+   <script type="text/javascript" src="js/calendario_dw.js"></script>
    
    <script type="text/javascript">
    $(document).ready(function(){
