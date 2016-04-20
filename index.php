8920cc69768befb2813fc5c1c47e5b788962f773
profeIAP <jasvazquez@iesalandalus.com>
2016-04-20 16:30:14 +0200

    Corrijo el estropicio de @FranPeroNoMoreno al subir la tarea #10

--- index.php
+++ index.php
@@ -159,6 +159,7 @@ $app->group('/alumnos', function () use ($app) {
 		echo $twig->render('alumno.php');  
 	}); 
 });
+
 $app->group('/usuario', function () use ($app) {
 
     $app->get('/', function() use ($app){
@@ -294,6 +295,65 @@ $app->group('/partes', function () use ($app) {
 $app->get('/about', function() use ($app){
 	global $twig;
 	echo $twig->render('about.php');  
+}); 
+
+function import_csv_to_sqlite(&$pdo, $csv_path, $options = array())
+{
+	extract($options);
+	
+	if (($csv_handle = fopen($csv_path, "r")) === FALSE)
+		throw new Exception('Cannot open CSV file');
+		
+	if(!$delimiter)
+		$delimiter = ';';
+		
+	$table = (isset($table) && !empty($table)) ? $table : preg_replace("/[^A-Z0-9]/i", '', basename($csv_path));
+	
+	if(!isset($fields)){
+		$fields = array_map(function ($field){
+			return strtolower(preg_replace("/[^A-Z0-9]/i", '', $field));
+		}, fgetcsv($csv_handle, 0, $delimiter));
+	}
+	
+	$create_fields_str = join(', ', array_map(function ($field){
+		return "$field TEXT NULL";
+	}, $fields));
+	
+	$pdo->beginTransaction();
+	
+	$create_table_sql = "CREATE TABLE IF NOT EXISTS $table ($create_fields_str)";
+	
+	$pdo->exec($create_table_sql);
+	
+	$insert_fields_str = join(', ', $fields);
+	$insert_values_str = join(', ', array_fill(0, count($fields),  '?'));
+	$insert_sql = "INSERT INTO $table ($insert_fields_str) VALUES ($insert_values_str)";
+	$insert_sth = $pdo->prepare($insert_sql);
+	
+	$inserted_rows = 0;
+	while (($data = fgetcsv($csv_handle, 0, $delimiter)) !== FALSE) {
+		$insert_sth->execute($data);
+		$inserted_rows++;
+	}
+	
+	$pdo->commit();
+	
+	fclose($csv_handle);
+	
+	return array(
+			/*'table' => $table,
+			'fields' => $fields,
+			'insert' => $insert_sth,*/
+			'table' => $table,
+			'inserted_rows' => $inserted_rows
+		);
+}
+
+$app->get('/importar', function() use ($app){
+    global $twig;
+    $valores=import_csv_to_sqlite($app->db, "./model/Datos/alumnos", array("delimiter"=>","));
+    echo $twig->render('importar.php',$valores);
+      
 }); 
 
 // Ponemos en marcha el router
