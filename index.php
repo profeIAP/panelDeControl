548dbe7cae11deb655893722133460725c2bb217
profeIAP <jasvazquez@iesalandalus.com>
2016-05-05 18:36:48 +0200

    Limpio y reorganizo el código para facilitar su lectura

--- index.php
+++ index.php
@@ -101,13 +101,13 @@ if (password_verify('InFoRmAtIcA', $hash)) {
 
 $app->group('/alumnos', function () use ($app) {
 	
-	
 	$app->get('/importar', function() use ($app){
-    global $twig;
-    $valores=import_csv_to_sqlite($app->db, "./model/datos/alumnos", array("delimiter"=>","));
-    echo $twig->render('importar.php',$valores);
-      
-}); 
+		global $twig;
+		$valores=import_csv_to_sqlite($app->db, "./model/datos/alumnos", array("delimiter"=>","));
+		echo $twig->render('importar.php',$valores);
+		  
+	}); 
+	
     $app->get('/', function() use ($app){
 		global $twig;
 		
@@ -124,7 +124,6 @@ $app->group('/alumnos', function () use ($app) {
 			echo "hola";
 		}); 
 	});
-	
 	
 	$app->group('/anotaciones', function () use ($app) {
 		$app->get('/', function() use ($app){
@@ -190,7 +189,7 @@ $app->group('/alumnos', function () use ($app) {
 			$q = $pdo->prepare($sql);
 			$q->execute($valores);
 			
-			$app->redirect('/usuarios');
+			$app->redirect('/alumnos');
 		}
 		else
 		{
@@ -203,7 +202,7 @@ $app->group('/alumnos', function () use ($app) {
 		
 			// Mostramos un mensaje al usuario
 			
-			$app->redirect('/usuarios');
+			$app->redirect('/alumnos');
 		}
 	}); 
 
@@ -340,6 +339,7 @@ $app->group('/usuarios', function () use ($app) {
 		echo $twig->render('');  
 	}); 
 });
+
 $app->get('/contartabla', function() use ($app){
 	
 		global $twig;
@@ -352,6 +352,7 @@ $app->get('/contartabla', function() use ($app){
 		$valores=array('ntablas'=>$r);
 		echo $twig->render('tablas.php',$valores);  	
 	});
+
 $app->get('/about', function() use ($app){
 	global $twig;
 	echo $twig->render('about.php');  
@@ -406,7 +407,7 @@ function import_csv_to_sqlite(&$pdo, $csv_path, $options = array()){
 			'insert' => $insert_sth,*/
 			'table' => $table,
 			'inserted_rows' => $inserted_rows
-		);
+	);
 }
 
 
@@ -425,7 +426,6 @@ $app->get('/grafica', function() use ($app){
     echo $twig->render('grafica.php');  
 }); 
 
-
 $app->get('/upload', function() use ($app){
     global $twig;
     echo $twig->render('upload.php');
@@ -433,49 +433,49 @@ $app->get('/upload', function() use ($app){
 
 $app->post('/upload', function() use ($app){
 	$target_dir = "model/datos/";
-$target_file = $target_dir .basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtoupper(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
+	$target_file = $target_dir .basename($_FILES["fileToUpload"]["name"]);
+	$uploadOk = 1;
+	$imageFileType = strtoupper(pathinfo($target_file,PATHINFO_EXTENSION));
+	// Check if image file is a actual image or fake image
 
-/*if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
-}*/
+	/*if(isset($_POST["submit"])) {
+		$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+		if($check !== false) {
+		    echo "File is an image - " . $check["mime"] . ".";
+		    $uploadOk = 1;
+		} else {
+		    echo "File is not an image.";
+		    $uploadOk = 0;
+		}
+	}*/
 
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-// Allow certain file formats
-if($imageFileType != "CSV") {
-    echo "Sorry, only CSV files are allowed.";
-    $uploadOk = 0;
-}
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
-}
-	}); 
+	// Check if file already exists
+	if (file_exists($target_file)) {
+		echo "Sorry, file already exists.";
+		$uploadOk = 0;
+	}
+	// Check file size
+	if ($_FILES["fileToUpload"]["size"] > 500000) {
+		echo "Sorry, your file is too large.";
+		$uploadOk = 0;
+	}
+	// Allow certain file formats
+	if($imageFileType != "CSV") {
+		echo "Sorry, only CSV files are allowed.";
+		$uploadOk = 0;
+	}
+	// Check if $uploadOk is set to 0 by an error
+	if ($uploadOk == 0) {
+		echo "Sorry, your file was not uploaded.";
+	// if everything is ok, try to upload file
+	} else {
+		if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+		    echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+		} else {
+		    echo "Sorry, there was an error uploading your file.";
+		}
+	}
+}); 
 
 // Ponemos en marcha el router
 $app->run();
