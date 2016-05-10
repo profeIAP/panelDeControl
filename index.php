269e5c4999032e5e8f7dadad8eb0d84f7cc9f539
Maria <mariagarciamiura@gmail.com>
2016-05-10 12:59:00 +0200

    Subo cambios desde #193

--- index.php
+++ index.php
@@ -131,9 +131,10 @@ $app->group('/alumnos', function () use ($app) {
 			global $twig;
 			// Espacio "dedicado" a juan carlos
 		}); 
-			$app->get('/crear', function() use ($app){
+		$app->get('/crear', function() use ($app){
 			global $twig;
 			echo $twig->render('anotacion.php'); 
+		});
 	});
 	
 	$app->get('/borrar', function() use ($app){
