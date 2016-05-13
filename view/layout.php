6331e21f64af40f9c401973b26bc400d4ef5e64e
marialuisapf <m.luisapascual98@gmail.com>
2016-05-13 13:26:51 +0200

    arreglo tarea #162

--- view/layout.php
+++ view/layout.php
@@ -37,7 +37,7 @@
     <script type="text/javascript" src="https://www.google.com/jsapi"></script>
     <link href="http://netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 	<link href="/css/menulateral.css" rel="stylesheet" type="text/css">
-
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <!--[if IE]>
         <script src="https://cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script>
