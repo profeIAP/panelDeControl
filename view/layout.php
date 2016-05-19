5214d86f6b1cefba936c466e4b9e77d8894c9007
profeIAP <jasvazquez@iesalandalus.com>
2016-05-19 18:32:10 +0200

    Limpio el menú lateral para facilitar el trabajo a @MartaCorzo

--- view/layout.php
+++ view/layout.php
@@ -14,6 +14,7 @@
 	<link href="/css/jqueryui.css" type="text/css" rel="stylesheet"/>
 
 	<script src="/js/jquery.min.js"></script>
+	
     {% block cabecera %}
 			
 			
@@ -132,224 +133,63 @@
         =========================== -->
         <div class="col-md-3 mainright">
             <div class="row">
-                <div class="col-sm-1 col-md-2 mobmid">
+				
+				<div class="col-sm-1 col-md-2 mobmid">
                     <span class="secicon fa fa-briefcase"></span>
                 </div><!--icon end-->
-
-                <div class="col-sm-13 col-md-14 ">
+                
+                <div class="col-sm-13 col-md-10 ">
+					
                     <h3 class="mobmid">MENÚ</h3>
 
                    <li id="grafica"><a href="/grafica">Grafica Alumnos</a></li>
-                   
-
-<style>
-* {
-  margin: 0;
-  padding: 0;
-  -webkit-box-sizing: border-box;
-  -moz-box-sizing: border-box;
-  box-sizing: border-box;
-}
-
-
-
-ul { list-style-type: none; }
-
-a {
-  color: #b63b4d;
-  text-decoration: none;
-}
-
-/** =======================
- * Contenedor Principal
- ===========================*/
-
-
-h1 {
-  color: #FFF;
-  font-size: 24px;
-  font-weight: 400;
-  text-align: center;
-  margin-top: 80px;
-}
-
-h1 a {
-  color: #c12c42;
-  font-size: 16px;
-}
-
-.accordion {
-  width: 100%;
-  max-width: 360px;
-  margin: 30px auto 20px;
-  background: #FFF;
-  -webkit-border-radius: 4px;
-  -moz-border-radius: 4px;
-  border-radius: 4px;
-}
-
-.accordion .link {
-  cursor: pointer;
-  display: block;
-  padding: 15px 15px 15px 42px;
-  color: #4D4D4D;
-  font-size: 14px;
-  font-weight: 700;
-  border-bottom: 1px solid #CCC;
-  position: relative;
-  -webkit-transition: all 0.4s ease;
-  -o-transition: all 0.4s ease;
-  transition: all 0.4s ease;
-}
-
-.accordion li:last-child .link { border-bottom: 0; }
-
-.accordion li i {
-  position: absolute;
-  top: 16px;
-  left: 12px;
-  font-size: 18px;
-  color: #595959;
-  -webkit-transition: all 0.4s ease;
-  -o-transition: all 0.4s ease;
-  transition: all 0.4s ease;
-}
-
-.accordion li i.fa-chevron-down {
-  right: 12px;
-  left: auto;
-  font-size: 16px;
-}
-
-.accordion li.open .link { color: #b63b4d; }
-
-.accordion li.open i { color: #b63b4d; }
-
-.accordion li.open i.fa-chevron-down {
-  -webkit-transform: rotate(180deg);
-  -ms-transform: rotate(180deg);
-  -o-transform: rotate(180deg);
-  transform: rotate(180deg);
-}
-
-/**
- * Submenu
- -----------------------------*/
-
-
-.submenu {
-  display: none;
-  background: #444359;
-  font-size: 14px;
-}
-
-.submenu li { border-bottom: 1px solid #4b4a5e; }
-
-.submenu a {
-  display: block;
-  text-decoration: none;
-  color: #d9d9d9;
-  padding: 12px;
-  padding-left: 42px;
-  -webkit-transition: all 0.25s ease;
-  -o-transition: all 0.25s ease;
-  transition: all 0.25s ease;
-}
-
-.submenu a:hover {
-  background: #b63b4d;
-  color: #FFF;
-}
-</style>
-</head>
-
-<ul id="accordion" class="accordion">
-  <li>
-    <div class="link"></i>Alumnos<i class="fa fa-chevron-down"></i></div>
-    <ul class="submenu">
-						<li><a href="/alumnos/crear">Crear</a></li>
-						<li><a href="/alumnos">Listar</a></li>
-						<li><a href="/alumnos/importar">Importar</a></li>
+                   <ul id="accordion" class="accordion">
+						  <li>
+							<div class="link">Alumnos<i class="fa fa-chevron-down"></i></div>
+							<ul class="submenu">
+												<li><a href="/alumnos/crear">Crear</a></li>
+												<li><a href="/alumnos">Listar</a></li>
+												<li><a href="/alumnos/importar">Importar</a></li>
+											</ul>
+						  </li>
+						 
+						  <li>
+							<div class="link">Anotaciones<i class="fa fa-chevron-down"></i></div>
+							<ul class="submenu">
+							  <li><a href="/alumnos/anotaciones/crear">Crear</a></li>
+								</ul>
+								</li>
+						  
+						  <li>
+							<div class="link">Notificaciones<i class="fa fa-chevron-down"></i></div>
+							<ul class="submenu">
+							  <li><a href="/notificaciones">Listar</a></li>
+							</ul>
+						  </li>
+						  <li>
+							<div class="link">Partes<i class="fa fa-chevron-down"></i></div>
+							<ul class="submenu">
+							  <li><a href="/partes/crear">Crear</a></li>
+												<li><a href="/partes">Listar</a></li>
+							</ul>
+						  </li>
+						  <li>
+							<div class="link">Usuarios<i class="fa fa-chevron-down"></i></div>
+							<ul class="submenu">
+													<li><a href="/usuarios/crear">Crear</a></li>
+												<li><a href="/usuarios">Listar</a></li>
+							</ul>
+						  </li>
+						  
+						   <li>
+							<div class="link">Salir<i class="fa fa-chevron-down"></i></div>
+							<ul class="submenu">
+							</ul>
+						  </li>
 					</ul>
-  </li>
- 
-  <li>
-    <div class="link"></i>Anotaciones<i class="fa fa-chevron-down"></i></div>
-    <ul class="submenu">
-                        <li><a href="/alumnos/anotaciones/crear">Crear</a></li>
-		            </ul>
-  </li>
-  
-  <li>
-    <div class="link"></i>Notificaciones<i class="fa fa-chevron-down"></i></div>
-    <ul class="submenu">
-      <li><a href="/notificaciones">Listar</a></li>
-    </ul>
-  </li>
-  <li>
-    <div class="link"></i>Partes<i class="fa fa-chevron-down"></i></div>
-    <ul class="submenu">
-      <li><a href="/partes/crear">Crear</a></li>
-						<li><a href="/partes">Listar</a></li>
-    </ul>
-  </li>
-  <li>
-    <div class="link"></i>Usuarios<i class="fa fa-chevron-down"></i></div>
-    <ul class="submenu">
-      						<li><a href="/usuarios/crear">Crear</a></li>
-						<li><a href="/usuarios">Listar</a></li>
-
-						<li>Importar</li>
-					</ul>
-
-                    <li><a href="/logout">Salir</a></li>
-
-    </ul>
-  </li>
-  
-   <li>
-    <div class="link"></i>Salir<i class="fa fa-chevron-down"></i></div>
-    <ul class="submenu">
-    </ul>
-  </li>
-</ul>
-<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script> 
-<script>
-$(function() {
-	var Accordion = function(el, multiple) {
-		this.el = el || {};
-		this.multiple = multiple || false;
-
-		// Variables privadas
-		var links = this.el.find('.link');
-		// Evento
-		links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
-	}
-
-	Accordion.prototype.dropdown = function(e) {
-		var $el = e.data.el;
-			$this = $(this),
-			$next = $this.next();
-
-		$next.slideToggle();
-		$this.parent().toggleClass('open');
-
-		if (!e.data.multiple) {
-			$el.find('.submenu').not($next).slideUp().parent().removeClass('open');
-		};
-	}	
-
-	var accordion = new Accordion($('#accordion'), false);
-});
-</script>
-
-                    
+                
                 </div><!--info end-->
-            </div><!--tech skills end-->
-		</div><!--right end-->
-    </div><!--container end-->
-
-                </div><!--info end-->
+                
             </div><!--tech skills end-->
 		</div><!--right end-->
 
@@ -366,8 +206,6 @@ $(function() {
 	<script type="text/javascript" src="http://cdn.datatables.net/1.10.2/js/jquery.dataTables.min.js"></script>
 	<script type="text/javascript" src="/js/calendario_dw.js"></script>
 	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js"></script>
-	
-	
 
 	<script type="text/javascript">
 		$(document).ready(function(){
@@ -379,6 +217,34 @@ $(function() {
 			$('#myTable').dataTable();
 		});
 	</script>
+	<script>
+		$(function() {
+			var Accordion = function(el, multiple) {
+				this.el = el || {};
+				this.multiple = multiple || false;
+
+				// Variables privadas
+				var links = this.el.find('.link');
+				// Evento
+				links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+			}
+
+			Accordion.prototype.dropdown = function(e) {
+				var $el = e.data.el;
+					$this = $(this),
+					$next = $this.next();
+
+				$next.slideToggle();
+				$this.parent().toggleClass('open');
+
+				if (!e.data.multiple) {
+					$el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+				};
+			}	
+
+			var accordion = new Accordion($('#accordion'), false);
+		});
+</script>
 </body>
 
 </html>
