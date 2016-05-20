d30087847efaa7cfc97d69d9959f636c7a1bcb12
usuario <usuario@pc135-1.(none)>
2016-05-20 13:56:11 +0200

    tarea 251

--- view/layout.php
+++ view/layout.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/home/usuario/Vídeos/panelDeControl/img/favicon.ico" rel="icon" type="image/x-icon" />
     
     <link href="/css/font-awesome.css" rel="stylesheet" type="text/css">	
     <link rel="stylesheet" href="/css/bootstrap.css">
