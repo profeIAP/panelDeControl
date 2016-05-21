038bc321bf8e8b51fdf4113af9dcb3cb8f34e94b
profeIAP <jasvazquez@iesalandalus.com>
2016-05-21 10:37:09 +0200

    Corregido warning en LoginClave.php e incluida ayuda para tarea #254

--- controller/LoginClave.php
+++ controller/LoginClave.php
@@ -4,7 +4,7 @@ require_once	'controller/Login.php';
 
 class LoginClave extends Login {
 
-	public static function autenticar($usuario, $clave){
+	public static function autenticar($pdo, $usuario, $clave){
 		return ($usuario=="profeIAP" && $clave=="clave");
 	}
 }
