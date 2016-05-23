8a5cac5492315e4ecd27f2703b2d2167f7dfc461
rocío <rocio@pc175-1.(none)>
2016-05-23 10:24:18 +0200

    Tarea #258

--- view/alumno.php
+++ view/alumno.php
@@ -57,11 +57,22 @@
 			<textarea style="width:100%" rows="8" cols="50" class="form-control" id="curso" name="curso" >{{comentario.CURSO}}</textarea>
 		</div>
 		
+		<div class="form-group col-md-9">
+			
 		</div>
 		
+		<div class="form-group col-md-3">
+			
 		<button type="submit" class="btn btn-danger">Cancelar</button><a href="/alumnos/cancelar"></a>
 
 		<button type="submit" class="btn btn-success">Aceptar</button><a href="/alumnos/aceptar"></a>
+		
+		</div>
+		
+
+
+
+
 </form>
 
 {% endblock cuerpo %}
