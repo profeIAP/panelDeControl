b05bac5df73b54f89e4e11f3f1cf67bf38bb73e7
usuario <usuario@pc135-1.(none)>
2016-05-27 13:08:08 +0200

    favicon

--- view/layout.php
+++ view/layout.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
+    <link href="/img/favicon.ico" rel="icon" type="image/x-icon" />
     <link href="/css/font-awesome.css" rel="stylesheet" type="text/css">	
     <link rel="stylesheet" href="/css/bootstrap.css">
 	<link rel="stylesheet" href="/css/style.css">
