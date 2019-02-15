056845a915c1d0b464b66a9f7bf7331c867fb3fb
antoniovalle14 <antoniovalleamodeo@gmail.com>
2019-02-15 10:51:43 +0100

    Close #335

--- view/usuarios.php
+++ view/usuarios.php
@@ -18,47 +18,4 @@
 	
 </div>
 
-
-</div>
-
-{% for comentario in usuarios %}
-	
-		{% for campo, valor in comentario %}
-			{{campo}} : {{valor}} <br>
-		{% endfor %}
-		{% set url=utils.protegerURL('/usuarios/borrar?id=?id=' ~ comentario.ID )%}
-		<a href="/usuarios/borrar?id={{url}}"><img width="32px" src="http://findicons.com/files/icons/2226/matte_basic/32/trash_can1.png"></a>
-		{% set url=utils.protegerURL('/usuarios/editar?id=' ~ comentario.ID )%}
-		<a href="/usuarios/editar?id={{url}}"><img width="32px" src="http://findicons.com/files/icons/2226/matte_basic/32/document_edit.png"></a><br>
-		----------------<br>
-	{% endfor %}
-
-<table class="table table-bordered table-hover" id="temas">				
-	<thead>	
-		<tr>			
-			<th>ID</th>	
-			<th>Nombre</th>
-			<th>Email</th>
-			<th>Clave</th>
-			<th>Acciones</th>
-		</tr>	
-	</thead>
-	<tbody style=" .table-hover"><a href="/usuarios/editar?id={{comentario.ID}}"><img width="32px" src="http://findicons.com/files/icons/2226/matte_basic/32/document_edit.png"></a><br>
-<a href="/usuarios/borrar?id={{comentario.ID}}"><img width="32px" src="http://findicons.com/files/icons/2226/matte_basic/32/trash_can1.png"></a>
-		{% for comentario in usuarios %}
-			<tr>
-			{% for campo, valor in comentario %}
-				<td>{{valor}}</td>
-			{% endfor %}
-				<td>
-					{% set url=utils.protegerURL('/usuarios/borrar?id=?id=' ~ comentario.ID )%}
-					<a href="{{url}}"><img width="32px" src="http://findicons.com/files/icons/2226/matte_basic/32/trash_can1.png"></a>
-					{% set url=utils.protegerURL('/usuarios/editar?id=' ~ comentario.ID )%}
-					<a href="{{url}}"><img width="32px" src="http://findicons.com/files/icons/2226/matte_basic/32/document_edit.png"></a>			
-				</td>
-			</tr>
-		{% endfor %}
-	</tbody>
-</table>
 {% endblock cuerpo %}
-
