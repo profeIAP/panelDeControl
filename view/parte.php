3779b680a2afa3d171e1df1bec74d5ef3429c7cd
antoniovalle14 <antoniovalleamodeo@gmail.com>
2019-02-22 11:04:28 +0100

    Close #358

--- view/parte.php
+++ view/parte.php
@@ -16,8 +16,8 @@
 
 		<div class="row">
 			<div class="form-group col-md-8">
-				<label for="alumnoaImplicado">Alumno/a implicado</label>
-				<input type="text" class="form-control" id="alumnoaImplicado" value="">
+				<label for="alumnoaImplicado">Alumno/a implicado <span style="color:red">*</span></label>
+				<input type="text" class="form-control" id="alumnoaImplicado" required>
 			</div>
 			
 			<div class="form-group col-md-2 ">
